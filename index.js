7af8391048bf01881e35134609860eb7c67807cf
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-05-17 10:07:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let http = require('http');
+let url = require('url');
+let StringDecoder = require('string_decoder').StringDecoder;
+
+let server = http.createServer(function (req, res) {
+    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', data => {
+        buffer += decoder.write(data);
+    });
+    req.on('end', (end) => {
+        buffer += decoder.end();
+        res.end('Hello Node Js');
+        console.log(buffer);
+    });
+});
+
+server.listen(3000, function () {
+    console.log("the server is listening on port 3000 now ");
+});
